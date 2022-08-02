903402e091b336672faa7053b214789652bed478
Noor <noor.muradi@protonmail.com>
2022-08-02 18:12:48 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+res.send('<h1><center>Nodejs app workig!<center></h1>');
+});
+
+
+
+app.listen(3008, () => {
+  console.log('App listening on port 3008!');
+
+
+});
