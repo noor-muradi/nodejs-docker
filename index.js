95373cec243afb63888e31691dc1330affad45d8
noor-muradi <noor.muradi@protonmail.com>
2022-10-09 16:19:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send('<h1><center>Hello World!<center></h1>');
+res.send('<h1><center>Hello World!!!<center></h1>');
 });
 
 
